7add6c0e657a9cd3757f166ac1e531c50fa89f38
wfnuser <wfnuser@wfnusers-MacBook-Air.local>
2015-04-12 17:34:20 +0800

    the fix of the chosen date output.

--- process.php
+++ process.php
@@ -1,12 +1,6 @@
 <?
 	echo "Sucess";
 	echo "<br/>";
-	echo $_POST['date_push0'];
-	echo "<br/>";
-	echo $_POST['date_push1'];
-	echo "<br/>";
-	echo $_POST['date_push2'];
-	echo "<br/>";
 	echo $_POST['username'];
 	echo "<br/>";
     echo $_POST['userid'];
@@ -21,6 +15,32 @@
     for($i=0,$j=strlen($datestring[0]);$i<$j;$i++){
     	if($datestring[0][$i]=='1')
     	{
+    		echo "</br>"
+    		echo 2015;
+    		echo "</br>";
+    		echo (int)($i/31+1);
+    		echo "</br>";
+    		echo ($i%31)+1;
+    		echo "</br>";
+    	}
+	}
+	for($i=0,$j=strlen($datestring[1]);$i<$j;$i++){
+    	if($datestring[1][$i]=='1')
+    	{
+    		echo "</br>"
+    		echo 2016;
+    		echo "</br>";
+    		echo (int)($i/31+1);
+    		echo "</br>";
+    		echo ($i%31)+1;
+    		echo "</br>";
+    	}
+	}
+	for($i=0,$j=strlen($datestring[0]);$i<$j;$i++){
+    	if($datestring[2][$i]=='1')
+    	{
+    		echo "</br>"
+    		echo 2017;
     		echo "</br>";
     		echo (int)($i/31+1);
     		echo "</br>";
