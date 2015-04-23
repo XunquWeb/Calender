ac944aba649ea5e67b11e9db055440134c124799
cao123 <caozhechao@126.com>
2015-04-23 22:47:00 +0800

    ''里面不能再用''

--- submitdate.php
+++ submitdate.php
@@ -10,5 +10,5 @@
 	$ary=explode(" ",$data);
 	foreach($ary as &$x)mysql_query("INSERT INTO ct(id,dt)  VALUES (".$id.",".$x.")")or die("<script>window.location='error.html'</script>");
 	mysql_close($con);
-	echo '<script>window.location='success.html'</script>';
+	echo "<script>window.location='success.html'</script>";
 ?>
